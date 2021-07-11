27063b2391a5b0ec143cb19f2bbc37e86e3f2b61
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2021-07-11 22:57:34 +0200

    Remove unused import

--- lib/components/change_tip_widget.dart
+++ lib/components/change_tip_widget.dart
@@ -1,11 +1,9 @@
 import '../auth/auth_util.dart';
 import '../backend/backend.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ChangeTipWidget extends StatefulWidget {
   ChangeTipWidget({
--- lib/login_page/login_page_widget.dart
+++ lib/login_page/login_page_widget.dart
@@ -1,6 +1,5 @@
 import '../auth/auth_util.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import '../main_page/main_page_widget.dart';
 import 'package:flutter/material.dart';
